d53d55916fa491ae49a0841c7fd9f5b985abd211
Allan Egidio <allan.egidio@outlook.com>
2022-03-02 21:58:08 -0300

    playing with tests

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,8 +1,15 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+
   def hello do
     "Allan, The Alchemist!"
   end
 
+  @doc """
+    Returns a list of strings representing a deck of playing cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -24,10 +31,30 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+   @doc """
+    Determines whether a deck contains a given card
+
+    ## Examples
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should
+    be in the hand.
+
+    ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> { hand, deck } = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -3,6 +3,17 @@ defmodule CardsTest do
   doctest Cards
 
   test "greets the world" do
-    assert Cards.hello() == :world
+    assert Cards.hello() == "Allan, The Alchemist!"
+  end
+
+  test "create_deck makes 20 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 20
+  end
+
+  test "shufflinh a deck randomizes it" do
+    deck = Cards.create_deck
+    # assert deck != Cards.shuffle(deck)
+    refute deck == Cards.shuffle(deck)
   end
 end
