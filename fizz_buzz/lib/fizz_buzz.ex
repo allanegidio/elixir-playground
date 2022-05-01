11df9e782e803a1032bd8ecb34dfc58219c36630
Allan Egidio <allan.egidio@outlook.com>
2022-05-01 20:59:09 -0300

    add fizzbuzz

--- fizz_buzz/lib/fizz_buzz.ex
+++ fizz_buzz/lib/fizz_buzz.ex
@@ -1,15 +1,27 @@
 defmodule FizzBuzz do
   def build(file_name) do
-    # case File.read(file_name) do
-    #   { :ok, content } -> content
-    #   { :error, reason } -> reason
-    # end
-
     file_name
       |> File.read()
       |> handle_file_read()
   end
 
-  def handle_file_read({ :ok, result }), do: result
-  def handle_file_read({ :error, reason }), do: reason
+  def handle_file_read({ :ok, result }) do
+    result
+      |> String.split(",")
+      |> Enum.map(&convert_and_evaluate_numbers/1)
+  end
+
+  def handle_file_read({ :error, reason }) do
+    "Error reading the file: #{reason}"
+  end
+
+  def convert_and_evaluate_numbers(number) do
+    number = String.to_integer(number)
+    evaluate_number(number)
+  end
+
+  def evaluate_number(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+  def evaluate_number(number) when rem(number, 3) == 0, do: :fizz
+  def evaluate_number(number) when rem(number, 5) == 0, do: :buzz
+  def evaluate_number(number), do: number
 end
