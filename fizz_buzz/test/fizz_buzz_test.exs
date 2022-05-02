cb683c703f06ab5d791337395ab33ab7890c6d05
Allan Egidio <allan.egidio@outlook.com>
2022-05-01 21:13:49 -0300

    add test elixir

--- fizz_buzz/test/fizz_buzz_test.exs
+++ fizz_buzz/test/fizz_buzz_test.exs
@@ -1,10 +1,17 @@
 defmodule FizzBuzzTest do
   use ExUnit.Case
-  doctest FizzBuzz
 
-  test "greets the world" do
-    "bunda" |> IO.inspect()
-    IO.inspect binding()
-    assert FizzBuzz.hello() == :world
+  describe "build/1" do
+    test "when a valid file is provided, should return converted list" do
+      expected_response = {:ok, [1, 2, :fizz, 4, :buzz, :buzz, :fizzbuzz, :buzz, :fizzbuzz, :buzz, 23, :fizzbuzz]}
+
+      assert FizzBuzz.build("numbers.txt") == expected_response
+    end
+
+    test "when a invalid file is provided, should return an error" do
+      expected_response = {:error, "Error reading the file: enoent"}
+
+      assert FizzBuzz.build("invalid.txt") == expected_response
+    end
   end
 end
