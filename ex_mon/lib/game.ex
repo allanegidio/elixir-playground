8774112e355d543c668861425868fe910c113b89
Allan Egidio <allan.egidio@outlook.com>
2022-05-03 17:07:09 -0300

    playing with Agent

---
+++ ex_mon/lib/game.ex
@@ -0,0 +1,16 @@
+defmodule ExMon.Game do
+  use Agent
+
+  def start(computer, player) do
+    initial_value = %{computer: computer, player: player, turn: :player, status: :started}
+    Agent.start_link(fn -> initial_value end, name: __MODULE__)
+  end
+
+  def info do
+    Agent.get(__MODULE__, fn current_state -> current_state end)
+  end
+
+  def player do
+    Map.get(info(), :player)
+  end
+end
