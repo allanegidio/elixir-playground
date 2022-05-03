107db13568a3eda03cbd6544e29a587a17835d52
Allan Egidio <allan.egidio@outlook.com>
2022-05-03 20:03:29 -0300

    update opponent life and add attack message

--- ex_mon/lib/game/status.ex
+++ ex_mon/lib/game/status.ex
@@ -2,12 +2,24 @@ defmodule ExMon.Game.Status do
   alias ExMon.Game
 
   def print_round_message() do
-    IO.puts("\n ============ The game is started! ============ \n")
+    IO.puts("\n =========== The game is started! =========== \n")
     IO.inspect(Game.info())
     IO.puts("---------------------------")
   end
 
+  def print_move_message(:computer, :attack, damage) do
+    IO.puts("\n =========== The Player attacked the computer dealing #{damage}! =========== \n")
+  end
+
+  def print_move_message(:player, :attack, damage) do
+    IO.puts("\n =========== The Computer attacked the computer dealing #{damage}! =========== \n")
+  end
+
+  def print_move_message(:player, :heal, damage) do
+    IO.puts("\n =========== The Computer healed #{damage} itself! =========== \n")
+  end
+
   def print_wrong_move_message(movement) do
-    IO.puts("\n ============ Invalid Movement: #{movement}. ============ \n")
+    IO.puts("\n =========== Invalid Movement: #{movement}. =========== \n")
   end
 end
