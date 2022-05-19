b0ed254406e21c80d1d952094cae7d947f9ca8ba
Allan Egidio <allan.egidio@outlook.com>
2022-05-18 21:26:44 -0300

    fix

--- ex_mon_api/lib/ex_mon_api/poke_api/client.ex
+++ ex_mon_api/lib/ex_mon_api/poke_api/client.ex
@@ -12,8 +12,7 @@ defmodule ExMonApi.PokeApi.Client do
 
   defp handle_get({:ok, %Tesla.Env{status: 200, body: body}}), do: {:ok, body}
 
-  defp handle_get({:ok, %Tesla.Env{status: 404}}),
-    do: {:error, %{message: "Pokemon not found!", status: 404}}
+  defp handle_get({:ok, %Tesla.Env{status: 404}}), do: {:error, "Pokemon not found!"}
 
   defp handle_get({:error, _reason} = error), do: error
 end
