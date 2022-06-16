838ffe756d3812a3ad0781dfe3ae07bcc69f6d5e
Allan Egidio <allan.egidio@outlook.com>
2022-06-16 18:39:11 -0300

    testing comprehensions

---
+++ servy/lib/servy/map_brabo.ex
@@ -0,0 +1,11 @@
+defmodule MapBrabo do
+  def main() do
+    style = %{"width" => 10, "height" => 20, "border" => "2px"}
+
+    result = for {key, val} <- style, into: %{}, do: {String.to_atom(key), val}
+
+    IO.inspect(result)
+  end
+end
+
+MapBrabo.main()
