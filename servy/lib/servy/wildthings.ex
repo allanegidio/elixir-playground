c4834ae83b6e3e08b336463df84291735a78f054
Allan Egidio <allan.egidio@outlook.com>
2022-06-24 13:18:12 -0300

    add wildthings to use file from a json

--- servy/lib/servy/wildthings.ex
+++ servy/lib/servy/wildthings.ex
@@ -1,19 +1,14 @@
 defmodule Servy.Wildthings do
   alias Servy.Bear
 
+  @templates_path Path.expand("db/", __DIR__)
+
   def list_bears do
-    [
-      %Bear{type: "Brown", name: "Teddy", id: 1, hibernating: true},
-      %Bear{type: "Black", name: "Smokey", id: 2, hibernating: false},
-      %Bear{type: "Brown", name: "Paddington", id: 3, hibernating: false},
-      %Bear{type: "Grizzly", name: "Scarface", id: 4, hibernating: true},
-      %Bear{type: "Polar", name: "Snow", id: 5, hibernating: false},
-      %Bear{type: "Grizzly", name: "Brutus", id: 6, hibernating: false},
-      %Bear{type: "Black", name: "Rosie", id: 7, hibernating: true},
-      %Bear{type: "Panda", name: "Roscoe", id: 8, hibernating: false},
-      %Bear{type: "Polar", name: "Iceman", id: 9, hibernating: true},
-      %Bear{type: "Grizzly", name: "Kenai", id: 10, hibernating: false}
-    ]
+    @templates_path
+    |> Path.join("bears.json")
+    |> read_file()
+    |> Poison.decode!(as: %{"bears" => [%Bear{}]})
+    |> Map.get("bears")
   end
 
   def get_bear(id) when is_integer(id) do
@@ -24,5 +19,16 @@ defmodule Servy.Wildthings do
     id
     |> String.to_integer()
     |> get_bear()
+  end
+
+  defp read_file(path) do
+    case File.read(path) do
+      {:ok, contents} ->
+        contents
+
+      {:error, reason} ->
+        IO.inspect("Error reading #{path}: #{reason}")
+        "[]"
+    end
   end
 end
