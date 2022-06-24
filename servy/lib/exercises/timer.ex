81bc7befc8e59a0252d8d5571f91c5c4ac9bbf90
Allan Egidio <allan.egidio@outlook.com>
2022-06-24 13:17:36 -0300

    fix exercicses

---
+++ servy/lib/exercises/timer.ex
@@ -0,0 +1,14 @@
+defmodule Timer do
+  def remind(reminder, seconds) do
+    spawn(fn ->
+      :timer.sleep(seconds * 1000)
+      IO.puts(reminder)
+    end)
+  end
+end
+
+Timer.remind("Stand Up", 5)
+Timer.remind("Sit Down", 10)
+Timer.remind("Fight, Fight, Fight", 15)
+
+# :timer.sleep(:infinity)
