5ce52090e354ad3f23dbbe9fce505e8c3975582a
Allan Egidio <allan.egidio@outlook.com>
2022-06-25 14:02:16 -0300

    add random time sleep to video cam

--- servy/lib/servy/video_cam.ex
+++ servy/lib/servy/video_cam.ex
@@ -1,6 +1,7 @@
 defmodule Servy.VideoCam do
   def get_snapshot(camera_name) do
-    :timer.sleep(1000)
+    time = :rand.uniform(10_000)
+    :timer.sleep(time)
 
     "#{camera_name}-snapshot.jpg"
   end
