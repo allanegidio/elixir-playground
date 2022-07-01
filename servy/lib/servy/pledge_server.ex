d915f882bd8cf8c91daa9da5181d742acc170262
Allan Egidio <allan.egidio@outlook.com>
2022-07-01 18:04:52 -0300

    refactor using pattern machting and add handle call

--- servy/lib/servy/pledge_server.ex
+++ servy/lib/servy/pledge_server.ex
@@ -34,43 +34,39 @@ defmodule Servy.PledgeServer do
     IO.puts("\nWaiting for a message...")
 
     receive do
-      {sender, {:create_pledge, name, amount}} ->
-        {:ok, id} = send_pledge_to_service(name, amount)
-        most_recent_pledges = Enum.take(cache, 2)
-
-        new_cache = [{name, amount} | most_recent_pledges]
-
-        send(sender, {:response, id})
+      {sender, message} when is_pid(sender) ->
+        {response, new_cache} = handle_call(message, cache)
+        send(sender, {:response, response})
         listen_loop(new_cache)
-
-      {sender, :recent_pledges} ->
-        send(sender, {:response, cache})
-        listen_loop(cache)
-
-      {sender, :total_pledged} ->
-        total = Enum.map(cache, &elem(&1, 1)) |> Enum.sum()
-        send(sender, {:response, total})
-        listen_loop(cache)
 
       unexpected ->
         IO.puts("Unexpected messages: #{inspect(unexpected)}")
     end
   end
 
+  def handle_call(:recent_pledges, cache) do
+    {cache, cache}
+  end
+
+  def handle_call({:create_pledge, name, amount}, cache) do
+    {:ok, id} = send_pledge_to_service(name, amount)
+
+    most_recent_pledges = Enum.take(cache, 2)
+    new_cache = [{name, amount} | most_recent_pledges]
+
+    {id, new_cache}
+  end
+
+  def handle_call(:total_pledged, cache) do
+    total =
+      cache
+      |> Enum.map(&elem(&1, 1))
+      |> Enum.sum()
+
+    {total, cache}
+  end
+
   defp send_pledge_to_service(_name, _amount) do
     {:ok, "pledge-#{:rand.uniform(1000)}"}
   end
 end
-
-alias Servy.PledgeServer
-
-PledgeServer.start()
-
-PledgeServer.create_pledge("larry", 10)
-PledgeServer.create_pledge("moe", 20)
-PledgeServer.create_pledge("curly", 30)
-PledgeServer.create_pledge("daisy", 40)
-PledgeServer.create_pledge("grace", 50)
-
-IO.inspect(PledgeServer.recent_pledges())
-IO.inspect(PledgeServer.total_pledged())
