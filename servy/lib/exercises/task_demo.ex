865d092fc186e05c771a4744a8c3c96e101d37e0
Allan Egidio <allan.egidio@outlook.com>
2022-07-04 11:00:09 -0300

    improving comments

--- servy/lib/exercises/task_demo.ex
+++ servy/lib/exercises/task_demo.ex
@@ -2,6 +2,10 @@ defmodule TaskDemo do
   alias Servy.VideoCam
   alias Servy.Fetcher
 
+  # 1 - Nesse primeiro caso temos o uso do spawn para pegar a mensagem que e enviada para o processo.
+  #     Porem nesse exemplo existe um problema, o receive e thread block e como o snapshop esta gerando em background com um timer aleatorio
+  #     O resultado esta vindo desincronizado. O processo da Cam-2 pode ser feito primeiro e ser recebido no receive.
+
   # def main() do
   #   process_parent = self()
 
@@ -27,17 +31,25 @@ defmodule TaskDemo do
   #   [snapshot1, snapshot2, snapshot3]
   # end
 
-  # def main() do
-  #   pid1 = Fetcher.async(fn -> VideoCam.get_snapshot("cam-1") end)
-  #   pid2 = Fetcher.async(fn -> VideoCam.get_snapshot("cam-2") end)
-  #   pid3 = Fetcher.async(fn -> VideoCam.get_snapshot("cam-3") end)
+  # 2 - Nesse exemplo eu uso o modulo Fetcher onde ele usa o spawn em uma funcao anonima e retorna o process id
+  #     Dessa forma no get result, posso fazer o check do pattern matching com o process id e pegar o resultado do processo que gerou o snapshot
+  #     Sendo possivel agora ordernar os snapshots pq como sabemos o result e thread block e ele ira aguardar o resultado ordenado dos processos conforme o codigo
 
-  #   snapshot1 = Fetcher.get_result(pid1)
-  #   snapshot2 = Fetcher.get_result(pid2)
-  #   snapshot3 = Fetcher.get_result(pid3)
+  def main() do
+    pid1 = Fetcher.async(fn -> VideoCam.get_snapshot("cam-1") end)
+    pid2 = Fetcher.async(fn -> VideoCam.get_snapshot("cam-2") end)
+    pid3 = Fetcher.async(fn -> VideoCam.get_snapshot("cam-3") end)
 
-  #   [snapshot1, snapshot2, snapshot3]
-  # end
+    snapshot1 = Fetcher.get_result(pid1)
+    snapshot2 = Fetcher.get_result(pid2)
+    snapshot3 = Fetcher.get_result(pid3)
+
+    [snapshot1, snapshot2, snapshot3]
+  end
+
+  # 3 - Nesse exemplo eu deixo de trabalhar com minha implementacao de Fetcher e passo utilizar o padrao do Elixir, a lib chamada Task
+  #     Ao invez de trabalhar com o PID, a task trabalha com ela mesma, identificando a task e aguardando o resultado do processo em paralelo a outra thread.
+  #     Eu utilizou o atom :infinity nesse caso porque como o resultado do snapshot e aleatorio, as vezes supera os 5ms padrao da biblioteca Task.Supervised
 
   # def main() do
   #   task1 = Task.async(fn -> VideoCam.get_snapshot("cam-1") end)
@@ -51,6 +63,8 @@ defmodule TaskDemo do
   #   [snapshot1, snapshot2, snapshot3]
   # end
 
+  # 4 - Um exemplo elegante utilizando Pipe e a lib Enum
+
   # def main() do
   #   result =
   #     ["cam-3", "cam-2", "cam-1"]
@@ -59,6 +73,8 @@ defmodule TaskDemo do
 
   #   result
   # end
+
+  # 5 - Um exemplo rodando varias tasks ao mesmo tempo e recebendo o resultado da lista de tasks
 
   # def main() do
   #   tasks = [
@@ -69,6 +85,8 @@ defmodule TaskDemo do
 
   #   Task.await_many(tasks, :infinity)
   # end
+
+  # 6 - Exemplo usando comprehensions
 
   # def main() do
   #   snapshots = ["cam-3", "cam-2", "cam-1"]
@@ -81,21 +99,3 @@ defmodule TaskDemo do
   #   Task.await_many(tasks, :infinity)
   # end
 end
-
-# 1 - Nesse primeiro caso temos o uso do spawn para pegar a mensagem que e enviada para o processo.
-#     Porem nesse exemplo existe um problema, o receive e thread block e como o snapshop esta gerando em background com um timer aleatorio
-#     O resultado esta vindo desincronizado. O processo da Cam-2 pode ser feito primeiro e ser recebido no receive.
-
-# 2 - Nesse exemplo eu uso o modulo Fetcher onde ele usa o spawn em uma funcao anonima e retorna o process id
-#     Dessa forma no get result, posso fazer o check do pattern matching com o process id e pegar o resultado do processo que gerou o snapshot
-#     Sendo possivel agora ordernar os snapshots pq como sabemos o result e thread block e ele ira aguardar o resultado ordenado dos processos conforme o codigo
-
-# 3 - Nesse exemplo eu deixo de trabalhar com minha implementacao de Fetcher e passo utilizar o padrao do Elixir, a lib chamada Task
-#     Ao invez de trabalhar com o PID, a task trabalha com ela mesma, identificando a task e aguardando o resultado do processo em paralelo a outra thread.
-#     Eu utilizou o atom :infinity nesse caso porque como o resultado do snapshot e aleatorio, as vezes supera os 5ms padrao da biblioteca Task.Supervised
-
-# 4 - Um exemplo elegante utilizando Pipe e a lib Enum
-
-# 5 - Um exemplo rodando varias tasks ao mesmo tempo e recebendo o resultado da lista de tasks
-
-# 6 - Exemplo usando comprehensions
