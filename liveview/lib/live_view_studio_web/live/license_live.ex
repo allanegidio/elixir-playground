00d252e433e1456a4ed8754d1f5599758fa4cddb
Allan Egidio <allan.egidio@outlook.com>
2022-07-27 18:54:38 -0300

    add slider liveview

---
+++ liveview/lib/live_view_studio_web/live/license_live.ex
@@ -0,0 +1,49 @@
+defmodule LiveViewStudioWeb.LicenseLive do
+  use LiveViewStudioWeb, :live_view
+
+  alias LiveViewStudio.Licenses
+
+  import Number.Currency
+
+  def mount(_params, _session, socket) do
+    seats = 2
+    value = Licenses.calculate(seats)
+    socket = assign(socket, seats: seats, amount: value)
+
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <h1>Team License</h1>
+    <div id="license">
+      <div class="card">
+        <div class="content">
+          <div class="seats">
+            <img src="images/license.svg">
+            <span>
+              Your license is currently for
+              <strong><%= @seats %></strong> seats.
+            </span>
+          </div>
+          <form phx-change="update">
+            <input type="range" min="1" max="10"
+                  name="seats" value={@seats} />
+          </form>
+          <div class="amount">
+            <%= number_to_currency(@amount)%>
+          </div>
+        </div>
+      </div>
+    </div>
+    """
+  end
+
+  def handle_event("update", %{"seats" => seats}, socket) do
+    seats = String.to_integer(seats)
+
+    socket = assign(socket, seats: seats, amount: Licenses.calculate(seats))
+
+    {:noreply, socket}
+  end
+end
