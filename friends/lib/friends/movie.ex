f6d3021bb85562b65ed3af2338253403a6724ba7
Allan Egidio <allan.egidio@outlook.com>
2022-08-14 13:44:55 -0300

    playing with has_many and belongs_to from Ecto

---
+++ friends/lib/friends/movie.ex
@@ -0,0 +1,9 @@
+defmodule Friends.Movie do
+  use Ecto.Schema
+
+  schema "movies" do
+    field(:title, :string)
+    field(:tagline, :string)
+    has_many(:characters, Friends.Character)
+  end
+end
